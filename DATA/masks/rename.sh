1a6ea9cc59c82efc2d3653c7579acf62e989fbf5
zhangjing1997 <joeyzhang0401@gmail.com>
2020-03-18 04:43:28 +0800

    delete mistaken uploaded jpg and xml files

--- DATA/masks/rename.sh
+++
@@ -1,9 +0,0 @@
-# Rename all *_pixels0.png to *.png
-# for f in *.png; do
-#    mv -- "$f" "${f%_pixels0.png}.png"
-# done
-
-# Rename all *.png to *.jpg
-for f in *.png; do
-    mv -- "$f" "${f%.png}.jpg"
-done
